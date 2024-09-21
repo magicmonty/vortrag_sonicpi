28c1143eea66e0ff29cee51ed48bfda72137d6f5
Martin Gondermann <magicmonty@pagansoft.de>
2024-09-21 20:02:42 +0200

    Add latexmkrc and flake

---
+++ .latexmkrc
@@ -0,0 +1,5 @@
+$recorder = 1;
+$pdf_mode = 1;
+$pdflatex = "pdflatex --shell-escape %O %S";
+$out_dir = "./out";
+@default_files = ("livecoding.tex");
---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    self,
+    flake-utils,
+    nixpkgs,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            alejandra
+            python312Packages.pygments
+          ];
+        };
+      }
+    );
+}
